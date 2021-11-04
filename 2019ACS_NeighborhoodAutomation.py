7180651a1914bb7521c92bc486a469b9f540830c
jasmnbur <92106362+jasmnbur@users.noreply.github.com>
2021-11-03 20:22:18 -0400

    Update 2019ACS_NeighborhoodAutomation.py

--- 2019ACS_NeighborhoodAutomation.py
+++ 2019ACS_NeighborhoodAutomation.py
@@ -1,17 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 ## IMPORT LIBRARY ##
 import pandas as pd
 import requests
 import json
-
-
-# In[2]:
-
 
 ## REQUESTS LIBRARY ## can alter requests to fit unique analysis needs ##
 # Constructing API:
@@ -25,23 +15,15 @@ predicates = {}
 get_vars = ["NAME","DP05_0001E", "DP05_0037E", "DP05_0038E", "DP05_0024E"] 
 predicates["get"] = ",".join(get_vars)
 predicates["for"] = "public use microdata area:*"
-# alternate geography filter==> predicates["for"] = "county:005,047,061,081,085"
+# Borough filter==> predicates["for"] = "county:005,047,061,081,085"
 predicates["in"] = "state:36"
 response = requests.get(base_url, params=predicates)
 
 # Checking status of API response to the request ## 200 means request has been processed successfully
 response.status_code
 
-
-# In[3]:
-
-
 # Converting API response to a json
 data = response.json()
-
-
-# In[7]:
-
 
 # Translating the json file into a data frame with pandas
 import pandas as pd
@@ -50,10 +32,6 @@ df.columns = ["Neighborhood", "Total Pop", "White alone", "Black Alone", "65 yea
 
 # PUMA filter to only display NYC neighborhoods
 df[df['Neighborhood'].str.contains("NYC")]
-
-
-# In[8]:
-
 
 # Converting into integers
 df['Total Pop'] = df['Total Pop'].astype(int)
@@ -64,30 +42,12 @@ df['65 years and older'] = df['65 years and older'].astype(int)
 # Formulating the ratio of 65 year and older
 df['% 65 years and older'] = (df['65 years and older']/df['Total Pop'])*100
 
-
-# In[9]:
-
-
 # Display PUMA filtered NYC neighborhoods with new variable
 df[df['Neighborhood'].str.contains("NYC")]
-
-
-# In[10]:
-
 
 # Filter for a specific borough
 df[df['Neighborhood'].str.contains("Manhattan")]
 
-
-# In[11]:
-
-
 # Filter for a specific neighborhood
 df[df['Neighborhood'].str.contains("West Harlem")]
-
-
-# In[ ]:
-
-
-
 
